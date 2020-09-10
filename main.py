d9f2c204671587ef556fab90d7efe5ad697cd534
Pepijn Krijnsen <linux@krijnsen.com>
2020-09-10 22:01:28 +0100

    Progress, finally

--- main.py
+++ main.py
@@ -14,12 +14,14 @@
 # 1. User types entries into Title, Body, and Affected System fields - then hits Save
 # 2. Presenter retrieves the data, validates it - then sends to Core
 # 3. Core logic stores data in Database
-# 4. 
+# 4. Core sends a new dictionary to the Presenter
+# 5. Presenter builds a new page - then sends to View
 
 
 #################
 #### IMPORTS ####
 import datetime
+import random
 
 
 ######################
@@ -32,5 +34,19 @@ class core:
         return {"title": title, "body": body, "stamp": date_format,}
 
 
-# def main():
+###################
+#### PRESENTER ####
+class presenter:
 
+    def newTicket(json_data):
+        f = open("lib/UIDs.csv")
+        UIDs = f.read()
+        UID = str(random.randrange(10, 99, 1))
+        while UID in UIDs:
+            UID = str(random.randrange(10, 99, 1))
+        json_data["UID"] = UID
+        f.close()
+        f = open("lib/UIDs.csv", "a")
+        f.write(UID + ",")
+        f.close()
+        return json_data
--- tests.py
+++ tests.py
@@ -1,19 +1,26 @@
 import unittest
 import datetime
 import main
+import random
 
 class tests(unittest.TestCase):
     
-#    def testOne(self):
-#        self.assertEqual(main.core.storeNewBofr("new_title", "new_body"), {"title": "new_title", "body": "new_body",})
 
-    def testPassingVariablesWorks(self):
-        nt = "A bug is eating me!"
-        nb = "Please send help"
-        dt = datetime.datetime.now()
-        fd = dt.strftime("%Y%m%d")
-        self.assertEqual(main.core.storeNewBofr(nt, nb), {"title": nt, "body": nb, "stamp": fd,})
+#    def testPassingVariablesWorks(self):
+#        nt = "A bug is eating me!"
+#        nb = "Please send help"
+#        dt = datetime.datetime.now()
+#        fd = dt.strftime("%Y%m%d")
+#        self.assertEqual(main.core.storeNewBofr(nt, nb), {"title": nt, "body": nb, "stamp": fd,})
 
+    def testPresenterReceivesNewTicketData(self):
+        json_data = {"title": "nt", "body": "nb",}
+        for i in range(80):
+            main.presenter.newTicket(json_data)
+        f = open("lib/UIDs.csv")
+        UIDs = f.read()
+        print(UIDs)
+        f.close()
 
 if __name__ == "__main__":
     unittest.main()
