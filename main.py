0f59c70986088f880bf3aa266e7dd20055027f5c
Pepijn K <pepijn@krijnsen.com>
2020-09-10 07:02:29 +0100

    User story one

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
+###################
+#### STRUCTURE ####
+# 
+# Haughty core:
+#   business logic
+# 
+# Entity: database:
+#   stores and retrieves tickets
+# 
+# Presenter:
+#   Prepares data for output by the view
+# 
+# STORY I: user writes & saves new ticket
+# 1. User types entries into Title, Body, and Affected System fields - then hits Save
+# 2. Presenter retrieves the data, validates it - then sends to Core
+# 3. Core logic stores data in Database
+# 4. 
+
+
 #################
 #### IMPORTS ####
 import datetime
