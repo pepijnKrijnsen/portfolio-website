8e857283404d3e3a73fc52f6b9be5f1bde1eceb6
Pepijn Krijnsen <linux@krijnsen.com>
2020-09-12 21:44:26 +0100

    Don't remember

--- dev/serve.py
+++ dev/serve.py
@@ -4,14 +4,18 @@ import os
 parent = os.path.dirname(os.path.abspath(__file__))
 parent += "/website"
 
-print(parent)
-
 app = Bottle()
 
 @app.route("/")
 def serve_home():
-    parent = os.path.dirname(os.path.abspath(__file__))
-    parent += "/website"
     return static_file("index.html", root = parent)
+
+@app.route("<filename:re:.*\.css>")
+def css(filename):
+    return static_file(filename, root = parent)
+
+@app.route("<filename:re:.*\.html>")
+def static_html(filename):
+    return static_file(filename, root = parent)
 
 run(app, host = "localhost", port = 8080, debug = True)
